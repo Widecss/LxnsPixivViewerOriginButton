a6e2910baddc21bf542834ec572962622131ba0f
Widecss <13403908+Widecss@users.noreply.github.com>
2023-05-11 06:51:02 +0800

    Add files via upload

---
+++ 屏蔽 BiliBili 的广告.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         屏蔽 BiliBili 的广告
+// @namespace    Widecss
+// @version      1.0
+// @description  仅包含删除 BiliBili 首页出现的广告的功能
+// @author       Widecss
+// @match        https://www.bilibili.com/*
+// @icon         https://www.bilibili.com/favicon.ico
+// @connect      bilibili.com
+// @grant        GM_xmlhttpRequest
+// @license MIT
+// @run-at document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(()=>{
+        for (let card of document.getElementsByClassName('feed-card')) {
+            if (card.getElementsByClassName("bili-video-card__info--ad").length > 0) {
+                card.remove();
+            }
+        }
+    }, 1000);
+    setInterval(()=>{
+        for (let card of document.getElementsByClassName('bili-video-card is-rcmd')) {
+            if (card.getElementsByClassName("bili-video-card__info--ad").length > 0) {
+                card.remove();
+            }
+        }
+    }, 1000);
+})();
